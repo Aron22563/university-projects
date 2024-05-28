fef51cc0b76bbb6b2d309ad97f7803b342203220
Aron22563 <arontinkl@gmail.com>
2024-05-28 18:03:25 +0200

    Implement basic functionality for buffer and getdents64:dirent structs. Encounter unexpected output types.

--- OS/systemprogramming/ps.cpp
+++ OS/systemprogramming/ps.cpp
@@ -2,9 +2,40 @@
 #include<unistd.h> 
 #include<dirent.h> 
 #include<signal.h> 
+#include<fcntl.h>
+#include<cstdio>
+#include<cstring> 
 #include<sys/syscall.h>
+#define BUF_SIZE 4096
+struct linux_dirent {
+    unsigned long  d_ino;     /* Inode number */
+    unsigned long  d_off;     /* Offset to next linux_dirent */
+    unsigned short d_reclen;  /* Length of this linux_dirent */
+    char           pad;       // Zero padding byte
+    char           d_type;    // File type (only since Linux 2.6.4;
+                              // offset is (d_reclen - 1))
+    char           d_name[];  /* Filename (null-terminated) */
+                        /* length is actually (d_reclen - 2 -
+                           offsetof(struct linux_dirent, d_name) */
+};
+
 int main(){ 
-DIR* fd=opendir("/proc");
-char buffer[4096]; 
-syscall(217, fd, buffer, sizeof(buffer)); 
-}
+int fd; 
+char buffer[BUF_SIZE]; 
+size_t BytesRead;
+struct linux_dirent* CurrentStruct;
+fd=openat(AT_FDCWD,"/proc",O_DIRECTORY);
+ if(fd==-1)
+  throw std::runtime_error("Folder access did not work as intended"); 
+
+ while((BytesRead=syscall(SYS_getdents64, fd, buffer, BUF_SIZE))>0){
+   for(size_t offset=0; offset<BytesRead;){
+     CurrentStruct=reinterpret_cast<struct linux_dirent*>(buffer+offset);
+       if(CurrentStruct->d_type==DT_DIR){
+         std::cout <<"Name: " <<  CurrentStruct->d_name << std::endl; 
+       }
+     offset+=CurrentStruct->d_reclen; 
+     }
+ }
+                 close(fd); 
+
