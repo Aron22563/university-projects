9dce2085aa72c3d59b653e797c60e08f6d1453da
Aron22563 <arontinkl@gmail.com>
2024-05-29 09:54:27 +0200

    Fixed output of names. Avoiding d_type output due to unknown memory structure

--- OS/systemprogramming/ps.cpp
+++ OS/systemprogramming/ps.cpp
@@ -7,18 +7,21 @@
 #include<sys/syscall.h>
 
 #define BUF_SIZE 4096
+#define NAME_MAX_LEN 255
 struct linux_dirent {
     unsigned long  d_ino;     /* Inode number */
     unsigned long  d_off;     /* Offset to next linux_dirent */
     unsigned short d_reclen;  /* Length of this linux_dirent */
-    char           pad;       // Zero padding byte
-    char           d_type;    // File type (only since Linux 2.6.4;
-                              // offset is (d_reclen - 1))
     char           d_name[];  /* Filename (null-terminated) */
                         /* length is actually (d_reclen - 2 -
                            offsetof(struct linux_dirent, d_name) */
-};
+    /*
+    char           pad;       // Zero padding byte
+    char           d_type;    // File type (only since Linux 2.6.4;
+                              // offset is (d_reclen - 1))
+    */
 
+};
 int main(){ 
 int fd; 
 char buffer[BUF_SIZE]; 
@@ -29,20 +32,13 @@ if(fd==-1)
 throw std::runtime_error("Folder access threw an error"); 
 
 while((BytesRead=syscall(SYS_getdents64, fd, buffer, BUF_SIZE))>0){
-for(size_t offset=0; offset<BytesRead;){
-CurrentStruct=reinterpret_cast<struct linux_dirent*>(buffer+offset);
-//if(CurrentStruct->d_type==DT_DIR){
- std::cout << "Ino number: " << CurrentStruct->d_ino << ", "; 
- std::cout << "Offset: " << CurrentStruct->d_off << ", "; 
- std::cout << "Length: " << CurrentStruct->d_reclen << ", "; 
- std::cout << "Pad: " << CurrentStruct->pad << ", "; 
- std::cout << "Struct type: " <<  CurrentStruct->d_type << ", "; 
- std::cout << "Name: " <<  CurrentStruct->d_name << std::endl; 
-//}
-offset+=CurrentStruct->d_reclen; 
-     }
-   if(BytesRead==-1)
-    throw std::runtime_error("Directory entry access error"); 	   
+ for(size_t offset=0; offset<BytesRead;){
+  CurrentStruct=reinterpret_cast<struct linux_dirent*>(buffer+offset);
+  std::cout << "Name: " <<  CurrentStruct->d_name << std::endl; 
+  offset+=CurrentStruct->d_reclen; 
  }
+ if(BytesRead==-1)
+    throw std::runtime_error("Directory entry access error"); 	   
+}
   close(fd); 
 }
